8dda044ea9dad083ea4c7407e96121fdf519a402
ClancySong <clancysong@gmail.com>
2017-10-11 17:52:45 +0800

    Calculator 1.1

--- Advanced/Calculator/base.js
+++ Advanced/Calculator/base.js
@@ -1,53 +1,54 @@
-var mainFunc = function() {
-  var screen = document.getElementsByClassName('screen-inner')[0];
-  var numBtns = document.getElementsByClassName('btn-number');
-  for (var i = 0; i < numBtns.length; i++) {
-    numBtns[i].onclick = function() {
-      var num = this.getAttribute('value');
-      screen.firstChild.nodeValue += num;
-    };
-  }
-
-  var opraBtns = document.getElementsByClassName('btn-operator');
-  var opras = ['+', '-', '*', '/', '.'];
-  for (i = 0; i < opraBtns.length; i++) {
-    opraBtns[i].onclick = function() {
-      var screenValue = screen.firstChild.nodeValue;
-      if (screenValue && opras.indexOf(screenValue[screenValue.length - 1]) === -1) {
-        screen.firstChild.nodeValue += this.getAttribute('value');
-      }
-    };
-  }
-
-  var signBtn = document.getElementById('btn-sign');
-  signBtn.onclick = function() {
-    var screenValue = screen.firstChild.nodeValue;
-    if(screenValue[screenValue.length - 1] === '-') {
-      screen.firstChild.nodeValue = screenValue.slice(0, -1) + '+';
-    } else if (screenValue[screenValue.length - 1] === '+') {
-      screen.firstChild.nodeValue = screenValue.slice(0, -1) + '-';
-    } else {
-      screen.firstChild.nodeValue += '-';
+var mainFunc = function () {
+    var screen = document.getElementsByClassName('screen-inner')[0];
+    var numBtns = document.getElementsByClassName('btn-number');
+    for (var i = 0; i < numBtns.length; i++) {
+        numBtns[i].onclick = function () {
+            var num = this.getAttribute('value');
+            screen.firstChild.nodeValue += num;
+        };
     }
-  };
 
-  var clearBtn = document.getElementById('btn-clr');
-  clearBtn.onclick = function() {
-    screen.firstChild.nodeValue = '';
-  };
+    var opraBtns = document.getElementsByClassName('btn-operator');
+    var opras = ['+', '-', '*', '/', '.'];
+    for (i = 0; i < opraBtns.length; i++) {
+        opraBtns[i].onclick = function () {
+            var screenValue = screen.firstChild.nodeValue;
+            if (screenValue && opras.indexOf(screenValue[screenValue.length - 1]) === -1) {
+                screen.firstChild.nodeValue += this.getAttribute('value');
+            }
+        };
+    }
 
-  var delBtn = document.getElementById('btn-del');
-  delBtn.onclick = function() {
-    var screenValue = screen.firstChild.nodeValue;
-    screen.firstChild.nodeValue = screenValue.slice(0, -1);
-  };
+    var signBtn = document.getElementById('btn-sign');
+    signBtn.onclick = function () {
+        var screenValue = screen.firstChild.nodeValue;
+        if (screenValue[screenValue.length - 1] === '-') {
+            screen.firstChild.nodeValue = screenValue.slice(0, -1) + '+';
+        } else if (screenValue[screenValue.length - 1] === '+') {
+            screen.firstChild.nodeValue = screenValue.slice(0, -1) + '-';
+        } else {
+            screen.firstChild.nodeValue += '-';
+        }
+    };
 
-  var equalBtn = document.getElementById('btn-equal');
-  equalBtn.onclick = function() {
-    var screenValue = screen.firstChild.nodeValue;
-    var ans = eval(screenValue);
-    screen.firstChild.nodeValue = ans;
-  };
+
+    var clearBtn = document.getElementById('btn-clr');
+    clearBtn.onclick = function () {
+        screen.firstChild.nodeValue = '';
+    };
+
+    var delBtn = document.getElementById('btn-del');
+    delBtn.onclick = function () {
+        var screenValue = screen.firstChild.nodeValue;
+        screen.firstChild.nodeValue = screenValue.slice(0, -1);
+    };
+
+    var equalBtn = document.getElementById('btn-equal');
+    equalBtn.onclick = function () {
+        var screenValue = screen.firstChild.nodeValue;
+        var ans = eval(screenValue);
+        screen.firstChild.nodeValue = ans;
+    };
 };
 
 window.onload = mainFunc;
