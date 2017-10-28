0858dd787c6341422cd5c702276f11219aa6fc1f
ClancySong <clancysong@gmail.com>
2017-10-28 22:50:37 +0800

    TicTacToeGame

---
+++ Advanced/TicTacToeGame/dist/js/base.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var _MainLogic = require('./MainLogic');
+
+function main() {
+    var circle = document.getElementById('circle');
+    var cross = document.getElementById('cross');
+    var title = document.getElementById('title');
+    var mainLogic = new _MainLogic.MainLogic('o');
+
+    function resetGame() {
+        mainLogic.checkerboard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+        mainLogic.stepNumber = 0;
+        title.firstChild.nodeValue = 'Playing...';
+        for (var i = 0; i < 3; i += 1) {
+            for (var j = 0; j < 3; j += 1) {
+                var checker = document.getElementById('checker-' + i + '-' + j);
+                checker.firstElementChild.classList.remove('fa-close', 'fa-circle-o');
+            }
+        }
+        if (mainLogic.playerChessPiece === 'x') {
+            circle.classList.remove('btn-active');
+            mainLogic.aiMoveInChess();
+        }
+        if (mainLogic.playerChessPiece === 'o') {
+            cross.classList.remove('btn-active');
+        }
+    }
+
+    circle.onclick = function () {
+        title.firstChild.nodeValue = 'Playing...';
+        mainLogic = new _MainLogic.MainLogic('o');
+        resetGame();
+        circle.classList.add('btn-active');
+    };
+
+    cross.onclick = function () {
+        title.firstChild.nodeValue = 'Playing...';
+        mainLogic = new _MainLogic.MainLogic('x');
+        resetGame();
+        cross.classList.add('btn-active');
+    };
+}
+
+window.onload = main;
